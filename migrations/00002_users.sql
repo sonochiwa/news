d4e114d869794c9b714ad6b9f3bb4343986c9ef8
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-05-12 22:58:57 +0600

    Убрал таймзону у TIMESTAMP

--- migrations/00002_users.sql
+++ migrations/00002_users.sql
@@ -2,12 +2,12 @@
 CREATE TABLE users
 (
     id            SERIAL PRIMARY KEY,
-    username      VARCHAR(50) UNIQUE      NOT NULL,
-    email         VARCHAR(320) UNIQUE     NOT NULL,
-    password_hash VARCHAR(60)             NOT NULL,
+    username      VARCHAR(50) UNIQUE                     NOT NULL,
+    email         VARCHAR(320) UNIQUE                    NOT NULL,
+    password_hash VARCHAR(60)                            NOT NULL,
     image_id      INTEGER,
-    created_at    TIMESTAMP DEFAULT now() NOT NULL,
-    deleted_at    TIMESTAMP,
+    created_at    TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    deleted_at    TIMESTAMP WITH TIME ZONE,
 
     FOREIGN KEY (image_id) REFERENCES images (id) ON DELETE SET NULL
 );
--- migrations/00010_posts.sql
+++ migrations/00010_posts.sql
@@ -3,9 +3,9 @@ CREATE TABLE posts
 (
     id         SERIAL PRIMARY KEY,
     image_id   INTEGER,
-    source_id  INTEGER                 NOT NULL,
-    created_at TIMESTAMP DEFAULT now() NOT NULL,
-    deleted_at TIMESTAMP,
+    source_id  INTEGER                                NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE,
 
     FOREIGN KEY (image_id) REFERENCES images (id),
     FOREIGN KEY (source_id) REFERENCES sources (id)
