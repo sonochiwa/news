d1eb437dff49d5f0fa5dcc60984f92cbd8f8c86c
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-05-16 14:02:12 +0600

    Подтянул верстку

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -20,7 +20,7 @@ func New(service services.Services) *gin.Engine {
 	router.Use(gin.Logger())
 
 	// Удалить потом
-	router.Static("/home/user/news/templates", "./templates/")
+	router.Static("/templates", "./templates/")
 
 	router.GET("/", func(c *gin.Context) {
 		c.Header("Content-Type", "text/html")
