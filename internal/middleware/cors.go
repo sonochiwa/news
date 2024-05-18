7fcac6f5a84cfa03c8fa8a15c1af8b3a3361495e
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-05-19 02:46:36 +0600

    Борьба с CORS

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -6,7 +6,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*,http://localhost:9000,https://news-4-you.ru,0.0.0.0:9000,http://0.0.0.0:9000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-API-Key,X-USER-ID,X-Admin-Key,Content-Type")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
