4ba318a8230de680a2e5df054d452eee26df2bf0
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-06-01 21:19:09 +0600

    добавил функционал выбора доступных стран

--- internal/repositories/countries/queries.go
+++ internal/repositories/countries/queries.go
@@ -2,7 +2,7 @@ package countries
 
 const (
 	getAllCountries = `
-select json_agg(rows) 
+select COALESCE(json_agg(row_to_json(rows)), '[]'::json)
 from (SELECT DISTINCT country as country_title, country_tag
 FROM posts) rows
 
