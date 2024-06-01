d3505b6c5e9dfbf3a923914cd85a996a2cae3634
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-06-01 21:20:39 +0600

    добавил функционал выбора доступных стран

--- internal/repositories/categories/queries.go
+++ internal/repositories/categories/queries.go
@@ -2,7 +2,7 @@ package categories
 
 const (
 	getAllCategories = `
-SELECT json_agg(row_to_json(categories))
+SELECT COALESCE(json_agg(row_to_json(categories)), '[]'::json)
 FROM (SELECT DISTINCT p.category as title
       FROM posts AS p
      ) categories;
